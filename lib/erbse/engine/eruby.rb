dacab837b960c1bbd67e9ab9a14952b4618253a1
Nick Sutterer <apotonick@gmail.com>
2015-05-25 11:31:22 +1000

    move code into Generator.

--- lib/erbse/engine/eruby.rb
+++ lib/erbse/engine/eruby.rb
@@ -2,11 +2,6 @@ require 'erbse/enhancer'
 
 
 module Erbse
-
-
-  ##
-  ## code generator for Ruby
-  ##
   module RubyGenerator
     include Generator
     #include ArrayBufferEnhancer
@@ -35,53 +30,15 @@ module Erbse
     #  src << "#{@bufvar} = [];"
     #end
     #++
-
-    def add_text(src, text)
-      src << " #{@bufvar} << '" << escape_text(text) << "';" unless text.empty?
-    end
-
-    def add_stmt(src, code)
-      #src << code << ';'
-      src << code
-      src << ';' unless code[-1] == ?\n
-    end
-
-    def add_expr_literal(src, code)
-      src << " #{@bufvar} << (" << code << ').to_s;'
-    end
-
-    def add_expr_escaped(src, code)
-      src << " #{@bufvar} << " << escaped_expr(code) << ';'
-    end
-
-    def add_expr_debug(src, code)
-      code.strip!
-      s = (code.dump =~ /\A"(.*)"\z/) && $1
-      src << ' $stderr.puts("*** debug: ' << s << '=#{(' << code << ').inspect}");'
-    end
-
-    #--
-    #def add_postamble(src)
-    #  src << "\n#{@bufvar}.join\n"
-    #end
-    #++
-
-  end
-
-
-  ##
-  ## engine for Ruby
-  ##
-  class Eruby < Basic::Engine
-    include RubyEvaluator
-    include RubyGenerator
-
     def add_preamble(src)
       puts "add_preamble called§§§§§§§§§§"
       @newline_pending = 0
       src << "@output_buffer = output_buffer;" # DISCUSS: i removed the output_buffer || ActionView::OB.new rubbish here.
     end
 
+    # def add_text(src, text)
+    #   src << " #{@bufvar} << '" << escape_text(text) << "';" unless text.empty?
+    # end
     def add_text(src, text)
       return if text.empty?
 
@@ -108,6 +65,20 @@ module Erbse
       end
     end
 
+    def ____add_stmt(src, code)
+      #src << code << ';'
+      src << code
+      src << ';' unless code[-1] == ?\n
+    end
+
+    def add_stmt(src, code)
+      flush_newline_if_pending(src)
+      ____add_stmt(src, code)
+    end
+
+    # def add_expr_literal(src, code)
+    #   src << " #{@bufvar} << (" << code << ').to_s;'
+    # end
     BLOCK_EXPR = /\s*((\s+|\))do|\{)(\s*\|[^|]*\|)?\s*\Z/
 
     def add_expr_literal(src, code)
@@ -128,15 +99,27 @@ module Erbse
       end
     end
 
-    def add_stmt(src, code)
-      flush_newline_if_pending(src)
-      super
+    # def add_expr_escaped(src, code)
+    #   src << " #{@bufvar} << " << escaped_expr(code) << ';'
+    # end
+
+    def add_expr_debug(src, code)
+      code.strip!
+      s = (code.dump =~ /\A"(.*)"\z/) && $1
+      src << ' $stderr.puts("*** debug: ' << s << '=#{(' << code << ').inspect}");'
     end
 
+    #--
+    #def add_postamble(src)
+    #  src << "\n#{@bufvar}.join\n"
+    #end
+    #++
     def add_postamble(src)
       flush_newline_if_pending(src)
       src << '@output_buffer.to_s'
     end
+
+
 
     def flush_newline_if_pending(src)
       if @newline_pending > 0
@@ -144,6 +127,16 @@ module Erbse
         @newline_pending = 0
       end
     end
+
+  end
+
+
+  ##
+  ## engine for Ruby
+  ##
+  class Eruby < Basic::Engine
+    include RubyEvaluator
+    include RubyGenerator
   end
 
 
@@ -161,28 +154,5 @@ module Erbse
   class EscapedEruby < Eruby
     include EscapeEnhancer
   end
-
-
-  ##
-  ## sanitize expression (<%= ... %>) by default
-  ##
-  ## this is equivalent to EscapedEruby and is prepared only for compatibility.
-  ##
-  class XmlEruby < Eruby
-    include EscapeEnhancer
-  end
-
-
-  class PI::Eruby < PI::Engine
-    include RubyEvaluator
-    include RubyGenerator
-
-    def init_converter(properties={})
-      @pi = 'rb'
-      super(properties)
-    end
-
-  end
-
 
 end
