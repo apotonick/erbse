5688bf5ddfdaa3d048aaf6a607b469e95036bc05
Nick Sutterer <apotonick@gmail.com>
2015-05-25 14:11:51 +1000

    add tests

---
+++ test/erbse_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+# most tests are via cells-erb
+
+class ErbseTest < MiniTest::Spec
+  it do
+    Erbse::Template.new(%{<%- text = "Hello" %> <%= text %>}).call.must_equal "@output_buffer = output_buffer; text = \"Hello\" ;@output_buffer.safe_append=' '.freeze;@output_buffer.append=( text );@output_buffer.to_s"
+  end
+
+  it do
+    Erbse::Template.new(%{<%= form_for do %><%= content_tag :div do %>DIV<% end %><% end %>}).call.must_equal "@output_buffer = output_buffer;@output_buffer.append=  form_for do @output_buffer.append=  content_tag :div do @output_buffer.safe_append='DIV'.freeze; end ; end ;@output_buffer.to_s"
+  end
+end
