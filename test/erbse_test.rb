a4d04bab1a3a4ce0bbe0c4c43a67018ab96efc35
Nick Sutterer <apotonick@gmail.com>
2015-05-27 11:51:14 +1000

    play with interpolated strings instead of all that output buffer work.

--- test/erbse_test.rb
+++ test/erbse_test.rb
@@ -28,4 +28,69 @@ class ErbseTest < MiniTest::Spec
 '.freeze;@output_buffer.safe_append='  '.freeze; call_me ;@output_buffer.safe_append='s
 '.freeze;@output_buffer.to_s}
   end
+
+
+  # play around with capture.
+  it do
+    string = %{Yeah <% res = capture do %>
+  Inside.
+  <%= method_call %>
+  Still Inside.
+<% end %> }
+
+    puts Erbse::Template.new(string).call
+
+    @output_buffer = output_buffer;@output_buffer.safe_append='Yeah '.freeze; res = capture do ;@output_buffer.safe_append='
+  Inside.
+'.freeze; end ;@output_buffer.safe_append=' '.freeze;@output_buffer.to_s
+
+  puts "@@@@@ #{@output_buffer}"
+
+
+
+
+
+  @output_buffer = output_buffer;
+
+  puts "interpolate"
+ puts  "Yeah
+  #{outer = "Outer"; nil}
+  #{res = capture do
+    "Inside.
+    #{outer}
+    #{method_call}
+    *#{res2 = capture do "between stars" end;nil}*
+    Still Inside. *#{res2}*"
+  end;nil } >cap; #{res} "
+
+
+#   @output_buffer.safe_append='Yeah '.freeze; res = capture do ;@output_buffer.safe_append='
+#   Inside.
+#   '.freeze;@output_buffer.append=( method_call );@output_buffer.safe_append='
+#   Still Inside.
+# '.freeze; end ;@output_buffer.safe_append=' '.freeze;@output_buffer.to_s
+
+
+puts
+
+  end
+
+  def output_buffer
+    OB.new
+  end
+
+  def capture(&block)
+    # here, we can execute the block in a different environment. disgusting.
+    yield
+  end
+
+  def method_call
+    "Yo!"
+  end
+
+  class OB < String
+    def safe_append=(str) # alias_method will be faster.
+      self << str
+    end
+  end
 end
