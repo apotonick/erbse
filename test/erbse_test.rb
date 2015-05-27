76b4e73f70bb7c5fb21c7c963e8dd4ea299058fd
Nick Sutterer <apotonick@gmail.com>
2015-05-27 10:18:53 +1000

    add a performance test. we still have more method calls than we need, for appending whitespaces.

--- test/erbse_test.rb
+++ test/erbse_test.rb
@@ -11,4 +11,21 @@ class ErbseTest < MiniTest::Spec
   it do
     Erbse::Template.new(%{<%= form_for do %><%= content_tag :div do %>DIV<% end %><% end %>}).call.must_equal "@output_buffer = output_buffer;@output_buffer.append=  form_for do @output_buffer.append=  content_tag :div do @output_buffer.safe_append='DIV'.freeze; end ; end ;@output_buffer.to_s"
   end
+
+  # performance optimizations.
+  it do
+    Erbse::Template.new(%{<b>Cool</b>
+     some text
+
+<b> Even cooler!</b>
+
+  <% call_me %>s
+}).call.must_equal %{@output_buffer = output_buffer;@output_buffer.safe_append='<b>Cool</b>
+     some text
+
+<b> Even cooler!</b>
+
+'.freeze;@output_buffer.safe_append='  '.freeze; call_me ;@output_buffer.safe_append='s
+'.freeze;@output_buffer.to_s}
+  end
 end
