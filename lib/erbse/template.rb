3008667558d9293c16bf40d002660ac36b1f21e2
Nick Sutterer <apotonick@gmail.com>
2016-07-31 17:49:22 +0200

    use Engine in Template.

--- lib/erbse/template.rb
+++ lib/erbse/template.rb
@@ -2,9 +2,10 @@ module Erbse
   # Compiles the runtime method for an ERB input string.
   class Template
     def initialize(input, properties={})
-      generator = RubyGenerator.new
-      parser    = Basic::Parser.new(properties, generator)
-      @src      = parser.call(input)
+      # generator = RubyGenerator.new
+      # parser    = Basic::Parser.new(properties, generator)
+
+      @src      = Engine.new.call(input)
     end
 
     def call
