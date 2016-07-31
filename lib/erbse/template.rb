9100f0e9e99e8c91f1199b5033e3ef94e13c7038
Nick Sutterer <apotonick@gmail.com>
2016-07-31 17:53:02 +0200

    cleanup template. should we simply alias Engine to Template?

--- lib/erbse/template.rb
+++ lib/erbse/template.rb
@@ -2,10 +2,7 @@ module Erbse
   # Compiles the runtime method for an ERB input string.
   class Template
     def initialize(input, properties={})
-      # generator = RubyGenerator.new
-      # parser    = Basic::Parser.new(properties, generator)
-
-      @src      = Engine.new.call(input)
+      @src = Engine.new.call(input)
     end
 
     def call
